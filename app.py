c897107dfe07d4f7e38f674148bfc1f87ff073e9
Lukasz <l.sobocinski123@gmail.com>
2020-05-04 17:13:14 +0200

    C:/Program Files/Git/api/participate enabling user to participate in an event

--- app.py
+++ app.py
@@ -1,29 +1,52 @@
 from flask import Flask, render_template, request, redirect, url_for
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from db_setup import Base, Event
+from db_setup import Base, Event, Participate
 from flask import jsonify
+import random
 
 app = Flask(__name__)
 
 engine = create_engine('sqlite:///event-collection.db')
 Base.metadata.bind = engine
 
-def get_session():
-    DBSession = sessionmaker(bind=engine)
-    return DBSession()
-
 def getEvents():
-    session = get_session()
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
     events = session.query(Event).all()
     response =  jsonify(events= [e.serialize for e in events])
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
+# TODO check errors and make it indempotent- login for user would be the best
+def participate(event_id, user_name, user_surname):
+    code = random.randint(0,10**8)*10 + int(event_id)
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+
+    if not session.query(Event).filter_by(event_id = event_id).one():
+        return "<h1>404</h1><p>The resource could not be found.</p>", 404
+    
+    add_participation = Participate(event_id=event_id, user_name=user_name,user_surname=user_surname, code=code)
+    session.add(add_participation)
+    session.commit()
+    return jsonify({'reservation_number' : code})
+
 @app.route("/api/events", methods = ['GET'])
 def show_events():
     if request.method == 'GET':
         return getEvents()
+
+@app.route("/api/participate", methods = ['POST'])
+def registrate_to_event():
+    if request.method == 'POST':
+        event_id = request.get_json()['event_id']
+        user_name = request.get_json()['user_name']
+        user_surname = request.get_json()['user_surname']
+        print(request.get_json()['event_id'])
+        return participate(event_id, user_name, user_surname)
+
+@app.route("/api/participate", methods = ['DELETE'])
 
 @app.errorhandler(404)
 def page_not_found(e):
